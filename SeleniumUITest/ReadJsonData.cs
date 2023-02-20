7b1efdd97b94268e4a4079ac421e0af1b5c2123a
SeleniumProject <seleniumden@gmail.com>
2023-02-20 19:25:43 +0530

    read json with multiple objects

--- SeleniumUITest/ReadJsonData.cs
+++ SeleniumUITest/ReadJsonData.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using OpenQA.Selenium;
 using SeleniumUITest.BasePage;
 using SeleniumUITest.DataModel;
@@ -14,9 +15,9 @@ namespace SeleniumUITest
     /// Summary description for ReadJsonData
     /// </summary>
     [TestClass]
-    public class ReadJsonData : BaseClass
+    public class ReadJsonData
     {
-       
+
 
         #region Additional test attributes
         //
@@ -41,39 +42,55 @@ namespace SeleniumUITest
         #endregion
 
         [TestMethod]
+        public void ReadJsonDataMultipleObjects()
+        {
+            RegisterList userdata = JsonConvert.DeserializeObject<RegisterList>(File.ReadAllText(@"C:\Users\Slgadmin\source\repos\SeleniumUITest\SeleniumUITest\TestData\RegisterData.json"));
+
+            for (int i = 0; i < userdata.Register.Count; i++)
+            {
+                string fname = userdata.Register[i].FirstName.ToString();
+                string lname = userdata.Register[i].LastName.ToString();
+                string email = userdata.Register[i].Email.ToString();
+                string password = userdata.Register[i].Password.ToString();
+                Console.WriteLine("================================");
+                Console.WriteLine(fname +" - " + lname +" - "+email +" - "+password);
+            }
+        }
+
+        [TestMethod]
         public void TestMethod1()
         {
 
-            string readdatafromjson = File.ReadAllText(@"C:\Users\Slgadmin\source\repos\SeleniumUITest\SeleniumUITest\TestData\userdata.json");
-            var registerData = JsonSerializer.Deserialize<RegisterDataModel>(readdatafromjson);
+            //string readdatafromjson = File.ReadAllText(@"C:\Users\Slgadmin\source\repos\SeleniumUITest\SeleniumUITest\TestData\userdata.json");
+            //var registerData = JsonSerializer.Deserialize<RegisterDataModel>(readdatafromjson);
 
-            Console.WriteLine(registerData.FirstName);
-            Console.WriteLine(registerData.LastName);
-            Console.WriteLine(registerData.Email);
-            Console.WriteLine(registerData.Password);
+            //Console.WriteLine(registerData.FirstName);
+            //Console.WriteLine(registerData.LastName);
+            //Console.WriteLine(registerData.Email);
+            //Console.WriteLine(registerData.Password);
 
-            String title = driver.Title;
-            Assert.AreEqual(title, "Demo Web Shop");
-            // Click on Register link
+            //String title = driver.Title;
+            //Assert.AreEqual(title, "Demo Web Shop");
+            //// Click on Register link
 
-            driver.FindElement(By.ClassName("ico-register")).Click();
-            String registerTitle = driver.Title;
-            Assert.AreEqual(registerTitle, "Demo Web Shop. Register");
+            //driver.FindElement(By.ClassName("ico-register")).Click();
+            //String registerTitle = driver.Title;
+            //Assert.AreEqual(registerTitle, "Demo Web Shop. Register");
 
-            // Select the Male Gender
-            driver.FindElement(By.Id("gender-male")).Click();
-            // Enter firstname
-            driver.FindElement(By.Id("FirstName")).SendKeys(registerData.FirstName);
-            // Enter lastname
-            driver.FindElement(By.Id("LastName")).SendKeys(registerData.LastName);
-            // Enter email
-            driver.FindElement(By.Id("Email")).SendKeys(registerData.Email);
-            // Enter Password
-            driver.FindElement(By.Id("Password")).SendKeys(registerData.Password);
-            // Enter confirm Password
-            driver.FindElement(By.Id("ConfirmPassword")).SendKeys(registerData.Password);
-            // Click on Register button
-            driver.FindElement(By.Id("register-button")).Click();
+            //// Select the Male Gender
+            //driver.FindElement(By.Id("gender-male")).Click();
+            //// Enter firstname
+            //driver.FindElement(By.Id("FirstName")).SendKeys(registerData.FirstName);
+            //// Enter lastname
+            //driver.FindElement(By.Id("LastName")).SendKeys(registerData.LastName);
+            //// Enter email
+            //driver.FindElement(By.Id("Email")).SendKeys(registerData.Email);
+            //// Enter Password
+            //driver.FindElement(By.Id("Password")).SendKeys(registerData.Password);
+            //// Enter confirm Password
+            //driver.FindElement(By.Id("ConfirmPassword")).SendKeys(registerData.Password);
+            //// Click on Register button
+            //driver.FindElement(By.Id("register-button")).Click();
             // get the successfull text message
 
         }
---
+++ SeleniumUITest/RegisterTestWithMultipleData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using SeleniumUITest.BasePage;
+using SeleniumUITest.DataModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SeleniumUITest
+{
+    /// <summary>
+    /// Summary description for RegisterTestWithMultipleData
+    /// </summary>
+    [TestClass]
+    public class RegisterTestWithMultipleData :BaseClass
+    {
+       
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RegisterList userdata = JsonConvert.DeserializeObject<RegisterList>(File.ReadAllText(@"C:\Users\Slgadmin\source\repos\SeleniumUITest\SeleniumUITest\TestData\RegisterData.json"));
+
+            for (int i = 0; i < userdata.Register.Count; i++)
+            {
+                string fname = userdata.Register[i].FirstName.ToString();
+                string lname = userdata.Register[i].LastName.ToString();
+                string email = userdata.Register[i].Email.ToString();
+                string password = userdata.Register[i].Password.ToString();
+
+               // Click on Register link
+
+                driver.FindElement(By.ClassName("ico-register")).Click();
+                String registerTitle = driver.Title;
+                Assert.AreEqual(registerTitle, "Demo Web Shop. Register");
+
+                // Select the Male Gender
+                driver.FindElement(By.Id("gender-male")).Click();
+                // Enter firstname
+                driver.FindElement(By.Id("FirstName")).SendKeys(fname);
+                // Enter lastname
+                driver.FindElement(By.Id("LastName")).SendKeys(lname);
+                // Enter email
+                driver.FindElement(By.Id("Email")).SendKeys(email);
+                // Enter Password
+                driver.FindElement(By.Id("Password")).SendKeys(password);
+                // Enter confirm Password
+                driver.FindElement(By.Id("ConfirmPassword")).SendKeys(password);
+                // Click on Register button
+                driver.FindElement(By.Id("register-button")).Click();
+                // get the successfull text message
+
+                String message = driver.FindElement(By.ClassName("result")).Text;
+                Assert.AreEqual(message, "Your registration completed");
+
+                // Log out
+                driver.FindElement(By.ClassName("ico-logout")).Click();
+            }
+        }
+    }
+}
